0a051ccd6894d17da7fc3e66c6a6bbe796c21d04
stefenzzz <phenuthijara@gmail.com>
2024-10-17 20:41:06 +0800

    remove import.meta.client and set token in bearer header directly

--- nuxt/plugins/axios.js
+++ nuxt/plugins/axios.js
@@ -4,16 +4,13 @@ import axios from 'axios';
 export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig(); 
 
-  // check if on client side
-  const token = import.meta.client ? localStorage.getItem('token'): ''; 
-
   const axiosInstance = axios.create({
     baseURL: config.public.baseURL,
   });
 
   axiosInstance.interceptors.request.use(config => {
-    config.headers.Authorization = `Bearer ${token}`;
     config.headers.Accept = 'application/json';
+    config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
     return config;
   })
 
